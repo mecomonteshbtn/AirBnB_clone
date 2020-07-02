7cd09dac6181e98b1fbebb17e1d1a9447dbf81b7
Robinson Montes <1574@holbertonschool.com>
2020-07-02 02:33:13 +0000

    fixing test errors

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -41,7 +41,7 @@ class TestCity(unittest.TestCase):
         """
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['tests/test_models/test_city.py'])
-        self.assertEqual(result.total_errors, 1,
+        self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_module_docstring(self):
